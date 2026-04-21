07cc44d1af25825cccdcf06371e96b12fcb0c8f2
Suraj <surajpradhan656@gmail.com>
2026-04-22 00:31:49 +0530

    Fix: Added Tailwind plugin back to config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // Added this back
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
+    tailwindcss(), // Added this back
     react(),
     VitePWA({
       registerType: 'autoUpdate',
